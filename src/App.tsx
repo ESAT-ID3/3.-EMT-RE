28cb13edd55c7c04ddf68e05856b46dbcaa9466b
petergsme <garciasan@esat-alumni.com>
2025-06-22 21:36:50 +0200

    migrating src 1/2

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Outlet, useLocation } from 'react-router-dom';
+import { useScrollToSection } from './hooks/useScrollToSection';
+import { useCanonical } from './hooks/useCanonical';
+
+import classNames from 'classnames/bind';
+import theme from './App.module.scss';
+import { Navbar } from './components/ui/Navbar/Navbar';
+import { AppSection } from './components/common/Apps/AppSection';
+import { SingleBlock } from './components/layout/SingleBlock/SingleBlock';
+import { Footer } from './components/ui/Footer/Footer';
+import { FloatingBanner } from './components/common/Apps/floatingBanner/FloatingBanner';
+
+const cx = classNames.bind(theme);
+
+export const App: React.FC = () => {
+  useScrollToSection();
+  useCanonical();
+  useLocation();
+
+  const appSection = (
+    <SingleBlock id="apps" backgroundColor="pink" className={cx('extra-padding-top')}>
+      <AppSection />
+    </SingleBlock>
+  );
+
+  return (
+    <>
+      <Navbar />
+
+      <Outlet />
+
+      {location.pathname === '/' && <FloatingBanner targetSectionId="apps" />}
+
+      {location.pathname !== '/error' && appSection}
+
+      <Footer />
+    </>
+  );
+};
